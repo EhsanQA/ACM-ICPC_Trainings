059799f194e64e2cc9fdc1b7a9a7b394711611bb
EhsanQA <ehsanaghaji@gmail.com>
2021-04-08 18:12:13 +0430

    CamelCaser Added

---
+++ src/com/company/CamelCaser.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class CamelCaser {
+    static String toCamelCase(String s){
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 0; i < sb.length(); i++) {
+            if (sb.charAt(i) == '-' || sb.charAt(i) == '_') {
+                sb.deleteCharAt(i);
+                sb.setCharAt(i, Character.toUpperCase(sb.charAt(i)));
+            }
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(toCamelCase("The_Stealth_Warrior"));
+    }
+}
