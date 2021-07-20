060fbeba4f45e32a5b9108b576f42ba6e37a3ad8
Ehsan QarehAgahji <80197312+EhsanQA@users.noreply.github.com>
2021-07-20 22:29:24 +0430

    Update RoundNumbers.cpp

--- src/com/company/RoundNumbers.cpp
+++ src/com/company/RoundNumbers.cpp
@@ -3,71 +3,53 @@
 
 using namespace std;
 
-int count;
+int n;
 
-
-int nonZeroCounter(int n) {
-    int nonZeroCount = 0;
-    while (n != 0) {
-        if ((n % 10) > 0) {
-            nonZeroCount++;
+int counter(int k, int p) {
+    if (p == 0) {
+        int d = 0;
+        while (k != 0) {
+            d++;
+            k /= 10;
         }
-        n = n / 10;
+        return d;
     }
-    return nonZeroCount;
+    else {
+        int d = 0;
+        while (k != 0) {
+            if (k % 10 != 0) {
+                d++;
+            }
+            k /= 10;
+        }
+        return d;
+    }
+    
 }
 
-int zeroCounter(int n) {
-    int zeroCounter = 0;
-    while (n != 0) {
-        if ((n % 10) == 0) {
-            zeroCounter++;
-        }
-        n = n / 10;
-    }
-    return zeroCounter;
-}
-
-int biggestDigit(int n) {
-    int d = 0;
-    while (n != 0) {
-        if ((n % 10) > 0) {
-            d = n % 10;
-        }
-        n = n / 10;
-    }
-    return d;
-}
-
-void restPrinter(int n) {
-    int big = biggestDigit(n) * (pow(10, zeroCounter(n) + nonZeroCounter(n) - 1));
-    n -= big;
-    while (n != 0) {
-        if ((n % 10) > 0) {
-            cout << n % 10 << " ";
-        }
-        n = n / 10;
-    }
-    cout << "\n";
-}
 
 int main() {
-    cin >> count;
-    int inputs[count];
-    for (int i = 0; i < count; i++) {
-        cin >> inputs[i];
-    }
-    for (int i = 0; i < count; i++) {
-        int tmp = inputs[i];
-        int nonZeroCount = nonZeroCounter(tmp);
-        int zeroCount = zeroCounter(tmp);
-        cout << nonZeroCount << "\n";
-        cout << biggestDigit(tmp);
-        for (int j = 0; j < zeroCount + nonZeroCount - 1; j++) {
-            cout << 0;
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        int t;
+        cin >> t;
+        int d = counter(t, 0);
+        int tt = t;
+        cout << counter(t, 1) << endl;
+        for (int j = 0; j < d; j++) {
+            int r = tt % 10;
+            tt /= 10;
+            if (r != 0) {
+                cout << r;
+                for (int u = 0; u < j; u++) {
+                    cout << 0;
+                }
+                cout << " ";
+            }
+
         }
-        cout << " ";
-        restPrinter(tmp);
+        cout << endl;
     }
     return 0;
 }
