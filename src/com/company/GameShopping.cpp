a74373c8897cb0450804a3b09fcef4a8a29b08e5
Ehsan QarehAgahji <80197312+EhsanQA@users.noreply.github.com>
2021-07-23 16:08:19 +0430

    Create GameShopping.cpp

---
+++ src/com/company/GameShopping.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m, ci, ai, aCount;
+
+
+int main() {
+    cin >> n >> m;
+    int c[n];
+    int a[m];
+    for (int i = 0; i < n; i++) {
+        cin >> c[i];
+    }
+    for (int i = 0; i < m; i++) {
+        cin >> a[i];
+    }
+    for (ci = 0; ci < n; ci++) {
+        // cout << "TESTING: " << endl;
+        // cout << "THE A: " << a[ai] << endl;
+        // cout << "THE C: " << c[ci] << endl;
+        if (a[ai] >= c[ci]) {
+            ai++;
+            aCount++;
+        }
+    }
+    cout << aCount;
+    return 0;
+}
