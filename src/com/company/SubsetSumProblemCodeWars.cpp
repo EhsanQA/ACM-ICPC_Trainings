22548c70352b9219057093e6125a3b9520946807
Ehsan QarehAgahji <80197312+EhsanQA@users.noreply.github.com>
2021-08-28 12:00:00 +0430

    Update SubsetSumProblemCodeWars.cpp

--- src/com/company/SubsetSumProblemCodeWars.cpp
+++ src/com/company/SubsetSumProblemCodeWars.cpp
@@ -5,40 +5,41 @@ using namespace std;
 const int MAXN = 100000;
 
 int a[MAXN];
+vector<int> v;
 
-int getSum(int r, int l) {
-//   cout << "GET SUM: R: " << a[l + 1] << " L: " << a[r - 1] << endl;
-  return a[r + 1] - a[l - 1];
+int getSum(int l, int r) {
+  return a[r] - a[l] + v.at(l);
 }
 
 
-int maxSequence(const vector<int> arr){
+int maxSequence(const vector<int>& arr){
+  v = {};
   for (int i : arr) {
     cout << i << endl;
   }
+  bool allNegative = true;
+  for (int i : arr) {
+      v.push_back(i);
+      if (i > 0) {
+          allNegative = false;
+      }
+  }
+  if (allNegative)
+    return 0;
   int arrSize = arr.size();
   if (arrSize == 0)
     return 0;
-  int theMax = getSum(1, 1);
-//   cout << "FIRST MAX: " << theMax << endl;
-  a[1] = arr.at(0);
-  for (int i = 2; i < arrSize; i++) {
-    a[i] = a[i - 1] + arr.at(i - 1);
-  }
+  int theMax = getSum(0, 0);
+  a[0] = arr.at(0);
   for (int i = 1; i < arrSize; i++) {
+    a[i] = a[i - 1] + arr.at(i);
+  }
+  for (int i = 0; i < arrSize; i++) {
     for (int j = i; j < arrSize; j++) {
       if (getSum(i, j) > theMax) {
         theMax = getSum(i, j);
-        cout << "i: " << i << " j: " << j << endl;
       }
     }
   }
-//   for (int i : a) {
-//     if (i != 0)
-// //       cout << "THE AS: " << i << endl;
-//   }
-//   cout << "THE MAX: " << theMax << endl;
-  if (theMax < 0)
-    return 0;
   return theMax;
 }
