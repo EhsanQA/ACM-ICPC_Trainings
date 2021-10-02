e0e4aca7c83d8c62d795089fbd68c3b2bc4eb253
Ehsan QarehAgahji <80197312+EhsanQA@users.noreply.github.com>
2021-10-02 08:37:43 +0330

    Create QuickSort.cpp

---
+++ src/com/company/QuickSort.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int n;
+vector<int> a;
+
+int partition(int l, int r) {
+    int x = a[r];
+    int i = l - 1;
+    for (int j = l; j < r; j++) {
+        if (a[j] <= x) {
+            i++;
+            int temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+    }
+    i++;
+    int t = a[i];
+    a[i] = a[r];
+    a[r] = t;
+    return i;
+}
+
+
+void quickSort(int l, int r) {
+    if (l < r) {
+        int q = partition(l, r);
+        quickSort(l, q - 1);
+        quickSort(q + 1, r);
+    }
+}
+
+int main() {
+    cin >> n;
+    a.push_back(-1);
+    for (int i = 0; i < n; i++) {
+        int t;
+        cin >> t;
+        a.push_back(t);
+    }
+    quickSort(1, n);
+    for (int i = 1; i <= n; i++)
+        cout << a[i] << " ";
+    cout << endl;
+}
